a23385976b034176a47c3047f3d374a0ac870bd2
Cezar <cezar.coelho@sap.com>
2019-03-26 13:18:21 -0300

    Backlog Item #04 and #05 - Lobby and Start Screens

---
+++ Alien Care Act/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    enum PlayerClass
+    {
+        Engineer,
+        Scrapper
+    }
+
+    int player_class;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPlayerClass(int player_class, Sprite player_sprite)
+    {
+        //This will need to be refactored in the future, we should add a SpriteBuffer Class to handle the sprites while in the game
+        transform.Find("Sprite").GetComponent<Image>().sprite = player_sprite;
+        this.player_class = player_class;
+    }
+}
---
+++ Alien Care Act/Assets/Scripts/lobbyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lobbyManager : MonoBehaviour
+{
+    public GameObject host_panel, client_panel;
+
+    public void OnHostReady()
+    {
+        host_panel.GetComponent<Image>().color = Color.green;
+    }
+    public void OnClientReady()
+    {
+        client_panel.GetComponent<Image>().color = Color.green;
+    }
+    public bool CanStartGame()
+    {
+        if((host_panel.GetComponent<Image>().color   == Color.green) && 
+           (client_panel.GetComponent<Image>().color == Color.green))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
