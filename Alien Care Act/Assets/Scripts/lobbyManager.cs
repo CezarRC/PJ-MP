1b7041b7eb80a8742a4fa2abde578fe831b44a78
Cezar Roveda Coelho <donroveda@gmail.com>
2019-03-27 00:28:21 -0300

    Small changes to Lobby Screen and connection issues

---
+++ Alien Care Act/Assets/Scripts/lobbyManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lobbyManager : MonoBehaviour
+{
+    public GameObject host_panel, client_panel;
+    public string host_name, client_name;
+    public photonHandler pHandler;
+
+    public void OnHostReady()
+    {
+        host_panel.GetComponent<Image>().color = Color.green;
+    }
+    public void OnClientReady()
+    {
+        client_panel.GetComponent<Image>().color = Color.green;
+    }
+
+    public void OnJoinRoom(string player_name)
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            host_name = player_name;
+            host_panel.transform.Find("player_name").GetComponent<Text>().text = host_name;
+            host_panel.SetActive(true);
+        }
+        else
+        {
+            foreach (PhotonPlayer player in PhotonNetwork.playerList)
+            {
+                if (player.IsMasterClient)
+                {
+                    host_name = player.NickName;
+                }
+            }
+            host_panel.transform.Find("player_name").GetComponent<Text>().text = host_name;
+            host_panel.SetActive(true);
+            client_name = player_name;
+            client_panel.transform.Find("player_name").GetComponent<Text>().text = client_name;
+            client_panel.SetActive(true);
+        }
+    }
+
+    public void OnSomeoneJoined(PhotonPlayer player)
+    {
+        if (!player.IsMasterClient)
+        {
+            client_name = player.NickName;
+            client_panel.transform.Find("player_name").GetComponent<Text>().text = client_name;
+            client_panel.SetActive(true);
+        }
+    }
+
+    public void OnMasterLeftRoom()
+    {
+        if (!PhotonNetwork.isMasterClient) //Aqui eu já sou o Master Client ou não? Debugar para descobrir!
+        {
+            host_name = client_name;
+            host_panel.transform.Find("player_name").GetComponent<Text>().text = host_name;
+            client_panel.SetActive(false);
+            host_panel.GetComponent<Image>().color = Color.red;
+        }
+    }
+    public void OnClientLeftRoom()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            client_panel.SetActive(false);
+        }
+    }
+
+    public void OnClickLeftRoom()
+    {
+        pHandler.GetOutOfTheRoom();
+    }
+
+    public bool CanStartGame()
+    {
+        if((host_panel.GetComponent<Image>().color   == Color.green) && 
+           (client_panel.GetComponent<Image>().color == Color.green))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Deactivate()
+    {
+        host_panel.SetActive(false);
+        client_panel.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Alien Care Act/Assets/Scripts/photonConnect.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class photonConnect : MonoBehaviour
+{
+    public string versionName = "0.1";
+
+    public GameObject connectingPanel;
+
+    public GameObject connectionPanel;
+
+    public GameObject disconnectPanel;
+
+    private bool rejoin, quit;
+
+    public void connectToGameServers()
+    {
+        PhotonNetwork.ConnectUsingSettings(versionName);
+
+        Debug.Log("Connectiong to photon Game Servers...");
+        connectingPanel.SetActive(true);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("We are connected to master server");
+        connectingPanel.SetActive(false);
+        connectionPanel.SetActive(true);
+    }
+
+    private void OnJoinedLobby()
+    {
+        Debug.Log("On Joined Lobby");
+    }
+
+    private void OnConnectionFail(DisconnectCause cause)
+    {
+        if (PhotonNetwork.Server == ServerConnection.GameServer)
+        {
+            switch (cause)
+            {
+                // add other disconnect causes that could happen while joined
+                case DisconnectCause.DisconnectByClientTimeout:
+                    rejoin = true;
+                    break;
+                case DisconnectCause.ExceptionOnConnect:
+                    rejoin = false;
+                    break;
+                case DisconnectCause.Exception:
+                    rejoin = false;
+                    break;
+                default:
+                    rejoin = false;
+                    break;
+            }
+        }
+    }
+
+    private void OnDisconnectedFromPhoton()
+    {
+        Debug.Log("Disconnected from the Game Server...");
+
+        if (rejoin && !PhotonNetwork.ReconnectAndRejoin())
+        {
+            SceneManager.LoadScene("LostConnection", LoadSceneMode.Single);
+        }
+        else if(rejoin && PhotonNetwork.ReconnectAndRejoin())
+        {
+            Debug.Log("Reconnecting...");
+        }
+        if (quit)
+        {
+            disconnectPanel.SetActive(false);
+        }
+    }
+
+    private void OnFailedToConnectToPhoton()
+    {
+        Debug.Log("Failed to connect to Game Server");
+    }
+
+    public void DisconnectFromMasterServer()
+    {
+        rejoin = false;
+        quit = true;
+        PhotonNetwork.Disconnect();
+        disconnectPanel.SetActive(true);
+    }
+}
