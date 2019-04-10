bf634fbc6b857cb2d76c33d3943b641569d72db5
Cezar Roveda Coelho <donroveda@gmail.com>
2019-04-10 00:20:40 -0300

    Engineer Basics | Scrapper Basics
    Now we have Engineer and Scrapper shooting, moving, colliding with the level, synched in the network.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2);
+        GetComponent<Rigidbody2D>().AddForce(transform.right * bulletSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
