f6e9c31fb54df646620ec1ad0c9ae32003ab8ed4
GFritter <41646954+GFritter@users.noreply.github.com>
2019-04-10 15:56:15 -0300

    Update Bullet.cs

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -8,6 +8,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //need specification on what the bullet is destroying on start
         Destroy(gameObject, 2);
         GetComponent<Rigidbody2D>().AddForce(transform.right * bulletSpeed);
     }
